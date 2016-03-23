5b836e163c2d0330ab9d53b0d30bcdaceec84eaa
Salomon Smeke <ssmeke@luc.edu>
2016-03-22 22:37:40 -0500

    updated js skeleton and html template

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,22 @@
+function startYourEngines(){
+  var $wrapper = $(".wrapper");
+  var $display = $(".time");
 
+}
 
+//Sample function
+function tick(){
+
+}
+
+//Sample HTML interactions
+function updateDOM(parent){
+  for (var i = 0; i < 47; i++){
+    
+  }
+}
+
+//Sample event handler
 $(".time span").on("click", function(e) {
   startYourEngines();
 });
